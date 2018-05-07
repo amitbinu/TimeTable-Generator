e69add724e10c1099d751fa6fe60b45295b3848a
Amit Binu <binua@mcmaster.ca>
2018-05-07 11:39:08 -0400

    Added the functionality to drag any course

--- src/backend/public/javascripts/schedule.js
+++ src/backend/public/javascripts/schedule.js
@@ -92,9 +92,32 @@ $(document).ready(function () {
                         temp = temp.replace('.','');
                         $("#schedule"+counter+' .time'+temp + " .day" +day).remove();
                     }
+                    cell.draggable({
+                        revert:  function(dropped) {
+                            var $draggable = $(this),
+                                hasBeenDroppedBefore = $draggable.data('hasBeenDropped'),
+                                wasJustDropped = dropped && dropped[0].id == "droppable";
+                            if(wasJustDropped) {
+                                // don't revert, it's in the droppable
+                                return false;
+                            } else {
+                                if (hasBeenDroppedBefore) {
+                                    // don't rely on the built in revert, do it yourself
+                                    $draggable.animate({ top: 0, left: 0 }, 'slow');
+                                    return false;
+                                } else {
+                                    // just let the built in revert work, although really, you could animate to 0,0 here as well
+                                    return true;
+                                }
+                            }
+                        }
+                    });
                     cell.attr('rowspan',rowspan);
                    cell.html(name+'<br>'+core.room+'<br>'+core.supervisor);
                    cell.css('background',color);
+                  /* cell.draggable({
+                       addClasses:true
+                   });*/
                 })
             });
             counter++;
